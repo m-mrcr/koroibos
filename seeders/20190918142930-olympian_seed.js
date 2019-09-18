f5219c1893aeac2795d514dd80ee22db268f2115
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-18 10:28:28 -0600

    Refactor seeds

--- seeders/20190918142930-olympian_seed.js
+++ seeders/20190918142930-olympian_seed.js
@@ -2,38 +2,43 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Olympians', [{
+    return queryInterface.bulkInsert('Olympians', [
+    {
       id: 1,
       name: 'Martin Mercer',
       age: 30,
       sex: 'M',
       weight: 83,
+      height: 168,
       team: "United States",
       sport: "Athletics",
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
+    },
+    {
       id: 2,
       name: 'Kelsey Paul',
       age: 28,
       sex: 'F',
       weight: 63,
+      height: 173,
       team: "Cananda",
       sport: "Athletics",
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
+    },
+    {
       id: 3,
       name: 'Allan Babcock',
       age: 32,
       sex: 'F',
       weight: 86,
+      height: 180,
       team: "United States",
       sport: "Wrestling",
       createdAt: new Date(),
       updatedAt: new Date()
-    }
-    ], {});
+    }], {});
   },
 
   down: (queryInterface, Sequelize) => {
