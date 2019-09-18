d2a473b8c2e48d140f3d239a0a82aeafef191152
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-18 10:34:14 -0600

    Adds tests for olympian index

--- routes/api/v1/olympians.js
+++ routes/api/v1/olympians.js
@@ -1,52 +1,74 @@
 var express = require('express');
 var router = express.Router();
 var defaultHeader = ["Content-Type", "application/json"]
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
+var Event = require('../../../models').Event;
 var Olympian = require('../../../models').Olympian;
 var OlympianEvent = require('../../../models').OlympianEvent;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 router.get('/', async function(req, res) {
   try {
-    // res.setHeader(...defaultHeader);
     if (req.query.age === 'youngest') {
-      let age = await Olympian.min('age')
-    } else if (req.query.age === 'oldest') {
-      let age = await Olympian.max('age')
+      var age = await Olympian.min('age');
     }
-    if (age) {
-      let olympians = Olympian.findAll({
+    if (req.query.age === 'oldest') {
+      var age = await Olympian.max('age');
+    }
+
+    if (age !== undefined) {
+      Olympian.findAll({
         attributes: ['name', 'team', 'age', 'sport'],
         where: {age: age},
         include: [{
           model: OlympianEvent,
           where: {
-            medal: {[Op.not]: null}
+            medal: {[Op.not]: 'NA'}
           },
           required: false
         }]
-      });
+      })
+      .then(olympians => {
+        return olympianSerializer(olympians)
+      })
+      .then(serializedOlympians => {
+        res.setHeader(...defaultHeader);
+        res.status(200).send({olympians: serializedOlympians})
+      })
+      .catch(error => {
+        res.setHeader(...defaultHeader);
+        res.status(404).send({error})
+      })
     } else {
-      let olympians = Olympian.findAll({
+      Olympian.findAll({
         attributes: ['name', 'team', 'age', 'sport'],
         include: [{
           model: OlympianEvent,
           where: {
-            medal: {[Op.not]: null}
+            medal: {[Op.not]: 'NA'}
           },
           required: false
-        }]
-      });
+          }
+        ]
+      })
+      .then(olympians => {
+        return olympianSerializer(olympians)
+      })
+      .then(serializedOlympians => {
+        res.setHeader(...defaultHeader);
+        res.status(200).send({olympians: serializedOlympians})
+      })
+      .catch(error => {
+        res.setHeader(...defaultHeader);
+        res.status(404).send({error})
+      })
     }
-    let response = serializeOlympians(olympians)
-    console.log(olympians)
-    res.status(200).send(JSON.stringify(`olympians: ${response}`));
   } catch (error) {
     res.status(500).send({ error })
   }
 });
 
-function serializeOlympians(olympians) {
+function olympianSerializer(olympians) {
   return olympians.map(function (olympian){
     let serializer = {
       name: olympian.name,
