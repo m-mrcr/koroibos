af706dbe36818e0c597fe2eb9cb2adfff420d60b
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-18 11:01:41 -0600

    Reconfigure test file for global setup/teardown

--- app.spec.js
+++ app.spec.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const shell = require('shelljs');
 const request = require("supertest");
 
-describe('Olympians Endpoints', () => {
+describe('testing', () => {
   beforeAll(() => {
      shell.exec('npx sequelize db:create')
      shell.exec('npx sequelize db:migrate')
@@ -13,19 +13,20 @@ describe('Olympians Endpoints', () => {
      shell.exec('npx sequelize db:migrate:undo:all')
    });
 
-  it('should return a 200 status and all olympians', async () => {
-    return await request(app)
-    .get('/api/v1/olympians')
-    .then(response => {
-      expect(response.status).toBe(200)
-      expect(response.body.olympians.length).toBe(3)
-      expect(Object.keys(response.body.olympians[0])).toContain('name')
-      expect(Object.keys(response.body.olympians[0])).toContain('team')
-      expect(Object.keys(response.body.olympians[0])).toContain('age')
-      expect(Object.keys(response.body.olympians[0])).toContain('sport')
-      expect(Object.keys(response.body.olympians[0])).toContain('total_medals_won')
-    })
-  });
+  describe('Olympians Endpoints', () => {
+    it('should return a 200 status and all olympians', async () => {
+      return await request(app)
+      .get('/api/v1/olympians')
+      .then(response => {
+        expect(response.status).toBe(200)
+        expect(response.body.olympians.length).toBe(3)
+        expect(Object.keys(response.body.olympians[0])).toContain('name')
+        expect(Object.keys(response.body.olympians[0])).toContain('team')
+        expect(Object.keys(response.body.olympians[0])).toContain('age')
+        expect(Object.keys(response.body.olympians[0])).toContain('sport')
+        expect(Object.keys(response.body.olympians[0])).toContain('total_medals_won')
+      })
+    });
 
     it('should return a 200 status and youngest olympians', async () => {
       return await request(app)
@@ -52,7 +53,49 @@ describe('Olympians Endpoints', () => {
           expect(Object.keys(response.body.olympians[0])).toContain('age')
           expect(Object.keys(response.body.olympians[0])).toContain('sport')
           expect(Object.keys(response.body.olympians[0])).toContain('total_medals_won')
-        })
-    })
+      })
+    });
+  });
 
+  describe('Stats Endpoints', () => {
+    it('should return a 200 status and olympian stats', async () => {
+      return await request(app)
+      .get('/api/v1/olympian_stats')
+      .then(response => {
+        expect(response.status).toBe(200)
+        expect(Object.keys(response.body.olympian_stats)).toContain('total_competing_olympians')
+        expect(Object.keys(response.body.olympian_stats)).toContain('average_weight')
+        expect(Object.keys(response.body.olympian_stats.average_weight)).toContain('unit')
+        expect(Object.keys(response.body.olympian_stats.average_weight)).toContain('male_olympians')
+        expect(Object.keys(response.body.olympian_stats.average_weight)).toContain('female_olympians')
+        expect(Object.keys(response.body.olympian_stats)).toContain('average_age')
+      })
+    });
+  });
+
+  describe('Events Endpoints', () => {
+    it('should return a 200 status and events', async () => {
+      return await request(app)
+      .get('/api/v1/events')
+      .then(response => {
+        expect(response.status).toBe(200)
+        expect(response.body.events.length).toBe(2)
+        expect(Object.keys(response.body.events[0])).toContain('sport')
+        expect(Object.keys(response.body.events[0])).toContain('events')
+      })
+    });
+
+    it('should return a 200 status and metalists', async () => {
+      return await request(app)
+      .get('/api/v1/events/1/medalists')
+      .then(response => {
+        expect(response.status).toBe(200)
+        expect(response.body.medalists.length).toBe(2)
+        expect(Object.keys(response.body)).toContain('event')
+        expect(Object.keys(response.body)).toContain('medalists')
+        expect(response.body.medalists[0].name).toBe('Kelsey Paul')
+        expect(response.body.medalists[1].name).toBe('Martin Mercer')
+      })
+    });
+  });
 })
